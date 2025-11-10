d3c45b0b29bad5d3c1f518f60f377230b8bf02b3
D1lxrry <119131367+D1lxrry@users.noreply.github.com>
2025-11-10 19:31:58 +0000

    Add files via upload

---
+++ Up-To Class Diagram/UserRepository.java
@@ -0,0 +1,28 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class UserRepository {
+
+    /**
+     * Default constructor
+     */
+    public UserRepository() {
+    }
+
+
+
+    /**
+     * @param username 
+     * @param password 
+     * @return
+     */
+    public User findUser(String username, String password) {
+        // TODO implement here
+        return null;
+    }
+
+}
